e800c7ddeae2fd0960e4c67a519fb9e92bbb58de
Kumkvatti <hansen.feng50@gmail.com>
2019-01-26 15:35:06 +0200

    Add script to generate lootbox codes

---
+++ generate_lb_codes.py
@@ -0,0 +1,21 @@
+import secrets
+import string
+import numpy as np
+
+# Generate N random n character codes, and removes duplicates
+
+N = 10000
+n = 16
+
+code_list = [[] for _ in range(N)]
+
+for x in range(0,N):
+    s =  (''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(n)))
+    if s not in code_list:
+        code_list[x] = s
+
+print(len(code_list))
+
+with open('lb_codes.txt', 'w') as f:
+    for x in code_list:
+        f.write(x + '\n')
