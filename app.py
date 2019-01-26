82395b31714b1539aee3099e278812131f205e35
Kumkvatti <hansen.feng50@gmail.com>
2019-01-26 17:34:39 +0200

    Move titeenijaba to seperate file

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
+from titeenijaba import Titeenijaba
 import atexit
+import json
+
 
 app = Flask(__name__)
 
 # Just a list to keep track of lootboxes and used lootboxes
 lb_list = []
-users_df =
+users_list = [Titeenijaba("visa", "TiK", [1,2,3]), Titeenijaba("Hansen","TiK", [1,3,2])]
 
 def read_lb_file():
     with open('lb_codes.txt', 'r') as f:
         for line in f:
             lb_list.append(line.strip())
-
-class titeenijaba:
-    def __init__(self, name, team, equip_list):
-        self.name = name
-        self.team = team
-        self.equip_list = equip_list
 
 @app.route('/')
 def hello():
@@ -25,15 +22,28 @@ def hello():
 
 # Write a backup to file, called every 20 minutes in scheduler
 def write_backup():
-    return "TODO"
+    with open('lb_codes.txt', 'w') as f:
+        for x in lb_list:
+            f.write(x + '\n')
+
+    def obj_dict(obj):
+        return obj.__dict__
+    
+    with open('user_info.json', 'w') as f:
+        json.dump(users_list, f, default = obj_dict)
+            
 
 def check_lootbox(code):
     return code in lb_list
 
+# TODO Fix this, only temporary teset function
+def create_user(name):
+    return "TODO"
+    
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=write_backup, trigger = "interval", seconds = 1200)
+    scheduler.add_job(func=write_backup, trigger = "interval", seconds = 3)
     scheduler.start()
     read_lb_file()
     app.run()
---
+++ titeenijaba.py
@@ -0,0 +1,5 @@
+class Titeenijaba:
+    def __init__(self, name, team, equip_list):
+        self.name = name
+        self.team = team
+        self.equip_list = equip_list
