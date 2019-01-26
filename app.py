4dcc2ca08d6df0f35e88582a06a2c936b59b3f08
Kumkvatti <hansen.feng50@gmail.com>
2019-01-26 16:29:04 +0200

    Create titeenijaba class

--- app.py
+++ app.py
@@ -6,29 +6,36 @@ app = Flask(__name__)
 
 # Just a list to keep track of lootboxes and used lootboxes
 lb_list = []
+users_df =
 
 def read_lb_file():
     with open('lb_codes.txt', 'r') as f:
         for line in f:
             lb_list.append(line.strip())
 
+class titeenijaba:
+    def __init__(self, name, team, equip_list):
+        self.name = name
+        self.team = team
+        self.equip_list = equip_list
+
 @app.route('/')
 def hello():
     return "hello world"
 
-# Writing data to a backup file
+# Write a backup to file, called every 20 minutes in scheduler
 def write_backup():
     return "TODO"
 
 def check_lootbox(code):
-    return "TODO"
+    return code in lb_list
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(func=write_backup, trigger = "interval", seconds = 1200)
-scheduler.start()
 
 if __name__ == '__main__':
-    read_lb_file():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=write_backup, trigger = "interval", seconds = 1200)
+    scheduler.start()
+    read_lb_file()
     app.run()
 
 
